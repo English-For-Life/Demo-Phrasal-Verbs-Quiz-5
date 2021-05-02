716f684255c7f50134d44fea47c621217912ce53
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-02 15:06:43 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Taxes never seem to go ___ . The government exacts more and more from us.",
+        options:["away", "up", "down", "back"],
+        answer: 2
+    },
+    {
+        q:"We can't go ___ with the project without them. They are a crucial part.",
+        options:["up", "out", "down", "ahead"],
+        answer: 3
+    },
+    {
+        q:"The local festival will go ___ as planned next month.",
+        options:["ahead", "away", "over", "down"],
+        answer: 0
+    },
+    {
+        q:"When the headmaster entered, the teacher went ___ explaining the new topic.",
+        options:["away", "on", "out", "after"],
+        answer: 1
+    },
+    {
+        q:"Every morning my mum goes ___ my room to say 'Hello'.",
+        options:["on", "away", "into", "out"],
+        answer: 2
+    }
+]
